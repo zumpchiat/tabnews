67e4803c86d2c9212fe598c968207f16a00a4c1c
Renan Machado <zumpchiat@gmail.com>
2024-08-28 01:31:16 +0000

    page home 2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Loretinho ama o que? </h1>
+    return <h1><p>Minha princesa de 12 anos, a cada dia que passa, você floresce mais e mais. Ver você crescer é um presente que guardo no coração. Tenho tanto orgulho da menina inteligente, divertida e cheia de vida que você é. Continue sendo essa estrela que ilumina nossos dias. Te amo mais que tudo!</p></h1>
 }
 
 export default Home;
