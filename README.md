f95106bcc4415a2f9ceb9ab40e89fed09f32a03a
zumpchiat <zumpchiat@gmail.com>
2024-09-06 17:38:01 -0300

    add `database ` e `.env`

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # tabnews
 
-projeto teste
+Projeto base
+
+# Subir o banco de dados local
+
+sudo docker compose -f infra/compose.yaml up
+
+comando de acesso: (psql client)
+psql --host=localhost --port=5432 --user=postgres
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(req, res) {
+import database from "../../../../infra/database.js";
+
+async function status(req, res) {
+  const result = await database.query("select 1 + 1 as sum;");
+  console.log(result.rows);
+
   res.status(200).json({ chave: "São Paulo" });
   //res.status(200).send("São Paulo");
 }
