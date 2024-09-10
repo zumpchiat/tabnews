48c027de332294b8b35dc2cb55c10bf194a88d88
zumpchiat <zumpchiat@gmail.com>
2024-09-10 15:41:38 -0300

    add date iso e test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,10 @@
-import database from "infra/database.js";
+//import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("select 1 + 1 as sum;");
-  console.log(result.rows);
+  const updateAt = new Date().toISOString();
 
-  res.status(200).json({ chave: "São Paulo" });
-  //res.status(200).send("São Paulo");
+  res.status(200).json({
+    update_at: updateAt,
+  });
 }
-
 export default status;
