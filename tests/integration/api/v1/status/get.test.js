8abd3a139355f47aa03f8180657acf5d39b71c70
zumpchiat <zumpchiat@gmail.com>
2024-09-13 16:38:38 -0300

    Handles db ssl connection

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,7 +7,7 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parsedUpdateAt);
-  expect(responseBody.dependencies.database.version).toEqual("16.4");
-  expect(responseBody.dependencies.database.max_connections).toEqual(112);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.conections_activity).toEqual(1);
 });
