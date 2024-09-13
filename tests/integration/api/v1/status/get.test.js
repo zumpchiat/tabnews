fdb6d67ac91dc4872aab8e61b51c3ddbeba2819a
zumpchiat <zumpchiat@gmail.com>
2024-09-13 15:43:12 -0300

    connect com db Neon

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -7,14 +7,7 @@ test("GET to /api/v1/status should return 200", async () => {
 
   const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parsedUpdateAt);
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.version).toEqual("16.4");
+  expect(responseBody.dependencies.database.max_connections).toEqual(112);
   expect(responseBody.dependencies.database.conections_activity).toEqual(1);
 });
-
-// test.only("Teste de SQL Injection", async () => {
-//   await fetch("http://localhost:3000/api/v1/status?dbName=db_local");
-//   await fetch(
-//     "http://localhost:3000/api/v1/status?dbName='; select pg_sleep(4) --;",
-//   );
-// });
