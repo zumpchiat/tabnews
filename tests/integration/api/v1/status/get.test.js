1c93f6755da311f3632c3f8b57590eb5ab25ced2
zumpchiat <zumpchiat@gmail.com>
2024-09-17 00:02:05 -0300

    adds  endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -2,7 +2,7 @@ test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 
-  responseBody = await response.json();
+  const responseBody = await response.json();
   expect(responseBody.update_at).toBeDefined();
 
   const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
