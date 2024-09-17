706316a870ceb767f92b3118a2a59f371e33fc27
zumpchiat <zumpchiat@gmail.com>
2024-09-17 17:26:18 -0300

    Add homolog

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
 function Home() {
   return (
     <h1>
-      <p>
-        Minha princesa de 12 anos, a cada dia que passa, você floresce mais e
-        mais. Ver você crescer é um presente que guardo no coração. Tenho tanto
-        orgulho da menina inteligente, divertida e cheia de vida que você é.
-        Continue sendo essa estrela que ilumina nossos dias. Te amo mais que
-        tudo!
-      </p>
+      <p>homolog</p>
     </h1>
   );
 }
